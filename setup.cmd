83ac0d16a4a52b5076ba6462da56e9b303f6dffe
Nikita Paniukhin <git@npanuhin.me>
2025-03-23 22:40:21 +0100

    Added more files for the demo

--- setup.cmd
+++ setup.cmd
@@ -8,9 +8,14 @@ rem Explanation: "&& ^"" is needed, since this executable gets deleted because w
 git checkout main
 git branch -D branchA
 git checkout -b branchB branchA_outdated && ^
-echo It's dangerous to go alone! Take this. > file.txt && ^
-git add file.txt && ^
-git commit --no-gpg-sign -m "New commit on branch `branchB`" && ^
+echo It's dangerous to go alone! Take this. > file_in_root.txt && ^
+echo It's dangerous to go alone! Take this. > folder_in_root/folder_nested/file_in_folders.txt && ^
+echo Changed on branch B > folder_in_root/folder_nested/file_changed_only_on_branchB.txt && ^
+echo Not empty anymore > folder_in_root/empty_file.txt && ^
+git rm folder_in_root/folder_nested/deleted_file.txt && ^
+echo. > folder_in_root/folder_nested/created_file.txt && ^
+git add folder_in_root file_in_root.txt && ^
+git commit --no-gpg-sign -m "New commit on branch `branchB` (stored only locally)" && ^
 git checkout main
 
 echo:
